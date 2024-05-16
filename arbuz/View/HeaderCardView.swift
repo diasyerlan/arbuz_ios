7cfb95d9995b5d25f6a16920aec8a88c82b0e530
diasyerlan <git config --global user.email diasyerlan@gmail.com>
2024-05-16 23:59:08 +0500

    Card for header created

--- arbuz/View/HeaderCardView.swift
+++ arbuz/View/HeaderCardView.swift
@@ -8,11 +8,44 @@
 import SwiftUI
 
 struct HeaderCardView: View {
+    var product: Product
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            VStack {
+                HStack {
+                    Text(product.title)
+                        .font(.system(size: 20))
+                    Image(systemName: "heart")
+                        .font(.system(size: 20))
+                    
+                    
+                }
+                
+                Button(action: {}) {
+                    HStack (spacing: 30){
+                        Text("$\(product.price)").fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        Image(systemName: "plus")
+                            .foregroundColor(.green)
+                            .font(.system(size: 20, weight: .bold))
+                    }
+                }
+                .padding(EdgeInsets(top: 5, leading: 10, bottom: 5, trailing: 10))
+                .background(Color.black.opacity(0.08))
+                .foregroundColor(.black)
+                .cornerRadius(16)
+                
+            }
+            Spacer()
+            Text(product.description)
+                .font(.title3)
+                .fontWeight(.medium)
+                .multilineTextAlignment(.center)
+            
+        }
+        .padding(.horizontal)
     }
 }
 
 #Preview {
-    HeaderCardView()
+    Home()
 }
