6d9a4c19516d5f095caffd5f00c009e1c94abb0a
diasyerlan <git config --global user.email diasyerlan@gmail.com>
2024-05-16 23:56:57 +0500

    Horizontal and grid list added

--- arbuz/View/Home.swift
+++ arbuz/View/Home.swift
@@ -8,32 +8,76 @@
 import SwiftUI
 
 struct Home: View {
-    let columns = [
-            GridItem(.flexible()),
-            GridItem(.flexible())
-        ]
+    @StateObject var homeData = HomeViewModel()
+    
     var body: some View {
         ScrollView {
             LazyVStack(alignment: .leading, spacing: 15,pinnedViews: [.sectionHeaders], content: {
-                Section(header: HeaderView()) {
+                
+                GeometryReader { reader -> AnyView in
+                    let offset = reader.frame(in: .global).minY
+                    
+                    if -offset >= 0 {
+                        DispatchQueue.main.async {
+                            self.homeData.offset = -offset
+                        }
+                    }
+                    return AnyView (
+                        
+                        Image(homeData.product!.image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: UIScreen.main.bounds.width, height: 250 + (offset > 0 ? offset : 0)).cornerRadius(2).offset(y: (offset > 0 ? -offset : 0))
+                            
+                    )
+                    
+                }.frame(height: 250)
+                
+                Section(header: HeaderView(product: $homeData.product)) {
+                    CustomTitle(title: "Special Offer")
+                    
                     ScrollView(.horizontal, showsIndicators: false) {
                         HStack(spacing: 20) {
                             ForEach(products) { product in
-                                CardView(product: product)
+                                CardView(size: 130, product: product)
                             }
                         }
                     }.padding()
+                    
+                    Divider().padding(.top)
+                    CustomTitle(title: "More")
+
+                    LazyVGrid(columns: homeData.columns, spacing: 20, content: {
+                        ForEach(groceryProducts) { product in
+                            CardView(size: 160, product: product)
+                        }
+                    })
                         
                 }
             })
-            Spacer(minLength: 50)
-            LazyVGrid(columns: columns, spacing: 20, content: {
-                ForEach(groceryProducts) { product in
-                    CardView(product: product)
-                }
-            })
+                
             
         }
+        .overlay(
+            Color.white
+                .frame(height: UIApplication.shared.connectedScenes
+                            .compactMap { $0 as? UIWindowScene }
+                            .first?.windows.first?.safeAreaInsets.top ?? 0)
+                .ignoresSafeArea(.all, edges: .top).opacity(homeData.offset > 250 ? 1: 0),
+            alignment: .top)
+        .environmentObject(homeData)
+
+    }
+}
+
+struct CustomTitle: View {
+    var title: String
+    
+    var body: some View {
+        Text(title)
+            .font(.title2)
+            .fontWeight(.bold)
+            .padding(.leading)
     }
 }
 
